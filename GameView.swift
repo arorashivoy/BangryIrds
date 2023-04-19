b0267861e70d1bcb574930cbc6342e27bec4a018
Shivoy Arora <85881270+arorashivoy@users.noreply.github.com>
2023-04-19 21:46:09 +0530

    Tried reducing the complexity of the file

--- GameView.swift
+++ GameView.swift
@@ -9,11 +9,6 @@ import SwiftUI
 
 struct GameView: View {
     @EnvironmentObject var modelData: ModelData
-    @State private var tapOpacity: Double = 0
-    
-    init() {
-        MyFont.registerFonts()
-    }
     
     var body: some View {
         
@@ -27,53 +22,11 @@ struct GameView: View {
                     .environmentObject(modelData)
                     .padding()
                 
+                GameOverlay()
+                    .environmentObject(modelData)
                 
-                VStack {
-                    HStack {
-                        Text("Level: \(modelData.currLevel)")
-                            .font(.custom("angrybirds-regular", size: min(reader.size.width, reader.size.height) / 30, relativeTo: .body))
-                            .padding()
-                            .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
-                            .background(content: {
-                                RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
-                                    .overlay{
-                                        RoundedRectangle(cornerRadius: 15)
-                                            .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
-                                    }
-                            })
-                            .padding(reader.size.width / 30)
-                        
-                        Spacer()
-                        
-                        Text("Shoots: \(modelData.shootsLeft)")
-                            .font(.custom("angrybirds-regular", size: min(reader.size.width, reader.size.height) / 30, relativeTo: .body))
-                            .padding()
-                            .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
-                            .background(content: {
-                                RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
-                                    .overlay{
-                                        RoundedRectangle(cornerRadius: 15)
-                                            .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
-                                    }
-                            })
-                            .padding(reader.size.width / 30)
-                        
-                        
-                    }
-                    Spacer()
-                    
-                    Text(modelData.stageCreated ? "Tap To Shoot" : "Tap to Continue")
-                        .font(.custom("angrybirds-regular", size: min(reader.size.width, reader.size.height) / 20, relativeTo: .body))
-                        .foregroundColor(.white)
-                        .padding()
-                        .opacity(tapOpacity)
-                        .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: tapOpacity)
-                        .onAppear(perform: {
-                            tapOpacity = 1
-                        })
-                }
+                
+                
             }
         }
     }
