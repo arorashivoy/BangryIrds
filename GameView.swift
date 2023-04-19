2d49c1085810797c72cdb42c6ce00f6b6f25aa30
Shivoy Arora <85881270+arorashivoy@users.noreply.github.com>
2023-04-19 21:18:27 +0530

    broke down the overlay

--- GameView.swift
+++ GameView.swift
@@ -16,73 +16,77 @@ struct GameView: View {
     }
     
     var body: some View {
-        GeometryReader { reader in
-            ZStack {
-                // Background color
-                Color.init(red: 255/255, green: 237/255, blue: 207/255)
-                    .ignoresSafeArea()
-                
-                ZStack {
-                    RealityKitView()
-                        .environmentObject(modelData)
-                        .padding()
-                    
-                    
-                    VStack {
-                        HStack {
-                            Text("Level: \(modelData.currLevel)")
-                                .font(.custom("angrybirds-regular", size: reader.size.width / 30, relativeTo: .body))
-                                .padding()
-                                .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
-                                .background(content: {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
-                                        .overlay{
-                                            RoundedRectangle(cornerRadius: 15)
-                                                .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
-                                        }
-                                })
-                                .padding(reader.size.width / 30)
-                            
-                            Spacer()
-                            
-                            Text("Shoots: \(modelData.shootsLeft)")
-                                .font(.custom("angrybirds-regular", size: reader.size.width / 30, relativeTo: .body))
-                                .padding()
-                                .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
-                                .background(content: {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
-                                        .overlay{
-                                            RoundedRectangle(cornerRadius: 15)
-                                                .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
-                                        }
-                                })
-                                .padding(reader.size.width / 30)
-
-                            
-                        }
-                        Spacer()
-                        
-                        Text(modelData.stageCreated ? "Tap To Shoot" : "Tap to Continue")
-                            .font(.custom("angrybirds-regular", size: reader.size.width / 20, relativeTo: .body))
-                            .foregroundColor(.white)
-                            .padding()
-                            .opacity(tapOpacity)
-                            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: tapOpacity)
-                            .onAppear(perform: {
-                                tapOpacity = 1
-                            })
-                    }
-                }
-                
-                if modelData.tutorial {
-                    
-                }
-                
-                
-            }
-        }
+        RealityKitView()
+            .environmentObject(modelData)
+            .padding()
+        
+//        GeometryReader { reader in
+//            ZStack {
+//                // Background color
+//                Color.init(red: 255/255, green: 237/255, blue: 207/255)
+//                    .ignoresSafeArea()
+//
+//                ZStack {
+//                    RealityKitView()
+//                        .environmentObject(modelData)
+//                        .padding()
+//
+//
+//                    VStack {
+//                        HStack {
+//                            Text("Level: \(modelData.currLevel)")
+//                                .font(.custom("angrybirds-regular", size: reader.size.width / 30, relativeTo: .body))
+//                                .padding()
+//                                .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
+//                                .background(content: {
+//                                    RoundedRectangle(cornerRadius: 15)
+//                                        .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
+//                                        .overlay{
+//                                            RoundedRectangle(cornerRadius: 15)
+//                                                .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
+//                                        }
+//                                })
+//                                .padding(reader.size.width / 30)
+//
+//                            Spacer()
+//
+//                            Text("Shoots: \(modelData.shootsLeft)")
+//                                .font(.custom("angrybirds-regular", size: reader.size.width / 30, relativeTo: .body))
+//                                .padding()
+//                                .foregroundColor(Color.init(red: 254/255, green: 249/255, blue: 239/255))
+//                                .background(content: {
+//                                    RoundedRectangle(cornerRadius: 15)
+//                                        .foregroundColor(.init(red: 254/255, green: 166/255, blue: 170/255))
+//                                        .overlay{
+//                                            RoundedRectangle(cornerRadius: 15)
+//                                                .stroke(Color.init(red: 248/255, green: 197/255, blue: 199/255), lineWidth: 10)
+//                                        }
+//                                })
+//                                .padding(reader.size.width / 30)
+//
+//
+//                        }
+//                        Spacer()
+//
+//                        Text(modelData.stageCreated ? "Tap To Shoot" : "Tap to Continue")
+//                            .font(.custom("angrybirds-regular", size: reader.size.width / 20, relativeTo: .body))
+//                            .foregroundColor(.white)
+//                            .padding()
+//                            .opacity(tapOpacity)
+//                            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: tapOpacity)
+//                            .onAppear(perform: {
+//                                tapOpacity = 1
+//                            })
+//                    }
+//                }
+//
+//                if modelData.tutorial {
+//
+//                }
+//
+//
+//            }
+//        }
     }
 }
 
